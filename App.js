a4414ccf6e1f09d15b87a836139fb37d66d3b25e
Akashkum1 <90310494+Akashkum1@users.noreply.github.com>
2021-12-02 16:25:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,7 @@ import SplashScreen from 'react-native-splash-screen';
 import React, { useEffect } from 'react';
 import StackNavigator from './src/navigation/StackNavigation';
 import { Provider } from 'react-redux';
-import  Reducer from './src/redux/reducer';
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-
-
-
-export const store = createStore(Reducer, applyMiddleware(logger));
+import store from './src/redux/store';
 
 const App = () => {
   useEffect(() => {
